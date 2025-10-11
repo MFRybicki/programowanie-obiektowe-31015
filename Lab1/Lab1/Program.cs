9715fd93301d8b9e689b1ba33b2fcb378663b13c
Mateusz Rybicki <mrybicki@edu.cdv.pl>
2025-10-11 11:00:37 +0200

    Z1-3

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*Example 1 
 
-Console.WriteLine("Hello, World!");
+string password;
+do
+{
+    Console.WriteLine("Podaj haslo: ");
+    password = Console.ReadLine();
+}
+while (password != "admin123");
+Console.WriteLine("Zalogowano pomyslnie!");*/
+
+//Example 2
+/*
+int number;
+do
+{ 
+    Console.WriteLine("Podaj liczbe wieksza od zera: ");
+    number = Convert.ToInt32(Console.ReadLine());
+
+} while (number < 0);
+Console.WriteLine("Numer poprawny!");*/
+
+//Example 3
+
